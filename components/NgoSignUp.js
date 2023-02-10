10be4d58716365e7d7af3a12c05cdb580ef839dd
Nandan Vyas <vyasnandu123@gmail.com>
2023-02-11 00:29:47 +0530

    Created signUp for NGOs

--- components/NgoSignUp.js
+++ components/NgoSignUp.js
@@ -1,26 +1,162 @@
-import React from 'react'
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const NgoSignUp = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [plans, setPlans] = useState([""]);
+  const [history, setHistory] = useState([""]);
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log(plans);
+    console.log(history);
+  };
+
+  const addItem = () => {
+    setPlans([...plans, ""]);
+  };
+
+  const addHistory = () => {
+    setHistory([...history, ""]);
+  };
+
+  const updateItem = (index, value) => {
+    const newPlans = [...plans];
+    newPlans[index] = value;
+    setPlans(newPlans);
+  };
+
+  const updateHistory = (index, value) => {
+    const newHistory = [...history];
+    newHistory[index] = value;
+    setHistory(newHistory);
+  };
 
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2">
-      <input type="text" {...register("name", { required: true }) } placeholder="NGO Name" className='inputField'/>
-      <input type="email" {...register("email", { required: true })} placeholder="Email"  className='inputField'/>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col space-y-2"
+      >
+        <input
+          type="text"
+          {...register("name", { required: true })}
+          placeholder="NGO Name"
+          className="inputField"
+        />
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          placeholder="Email"
+          className="inputField"
+        />
 
-      <input type="tel" {...register("phoneNumber")} placeholder="Contact Number"  className='inputField'/>
-      <input type="file" {...register("donationPreference")} placeholder="Donation Preference" className='inputField'/>
-      
+        <input
+          type="tel"
+          {...register("phoneNumber")}
+          placeholder="Contact Number"
+          className="inputField"
+        />
+        <div className="flex flex-row ">
+          <div className=" rounded-sm border-b px-1 py-2">Target Field : </div>
+          <select {...register("target")} className="inputField">
+            <option value="generalSupport">General Support</option>
+            <option value="education">Education</option>
+            <option value="health">Health</option>
+            <option value="HandP">Hunger and Poverty</option>
+            <option value="environment">Environment</option>
+            <option value="humanitarianAid">Humanitarian Aid</option>
+            <option value="animals">Animals</option>
+            <option value="artsAndCulture">Arts and Culture</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
 
-      {errors.exampleRequired && <span>This field is required</span>}
-      
-      <button type='submit' onClick={handleSubmit(onSubmit)} className='font-bold text-md rounded-lg tracking-widest py-2 bg-green-300 '>Submit</button>
-    </form>
+        <div className="flex flex-col border border-red-400 p-1">
+          <div className="text-xl">History :</div>
+          {history.map((item, index) => (
+            <div className="mb-3" key={index}>
+              <input
+                className="inputField"
+                type="text"
+                value={item}
+                onChange={(e) => updateHistory(index, e.target.value)}
+              />
+              <button
+                className="bg-red-500 text-white mx-1 p-2 rounded-lg"
+                onClick={() => setHistory(history.filter((_, i) => i !== index))}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+          <button
+            className="bg-blue-500 text-white p-2 rounded-xl"
+            onClick={addHistory}
+          >
+            Add History
+          </button>
+        </div>
+
+        <input
+          type="text"
+          {...register("endGoal")}
+          placeholder="End Goal"
+          className="inputField"
+        />
+
+        <div className="flex flex-col border border-red-400 p-1">
+          <div className="text-xl">Future Plans :</div>
+          {plans.map((item, index) => (
+            <div className="mb-3" key={index}>
+              <input
+                className="inputField"
+                type="text"
+                value={item}
+                onChange={(e) => updateItem(index, e.target.value)}
+              />
+              <button
+                className="bg-red-500 text-white mx-1 p-2 rounded-lg"
+                onClick={() => setPlans(plans.filter((_, i) => i !== index))}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+          <button
+            className="bg-blue-500 text-white p-2 rounded-xl"
+            onClick={addItem}
+          >
+            Add Item
+          </button>
+        </div>
+
+        {/* <input type="text" {...register("plans")} placeholder="Plans"  className='inputField'/> */}
+
+        {/* <input type="file" {...register("donationPreference")} placeholder="Donation Preference" className='inputField'/> */}
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          placeholder="Password"
+          className="inputField"
+        />
+
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <button
+          type="submit"
+          onClick={handleSubmit(onSubmit)}
+          className="font-bold text-md rounded-lg tracking-widest py-2 bg-green-300 "
+        >
+          Submit
+        </button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default NgoSignUp
+export default NgoSignUp;
