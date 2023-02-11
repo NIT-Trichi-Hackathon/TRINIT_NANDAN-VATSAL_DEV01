c892ed4fddb100f9b85594cede1590c4ed2a6f1f
Nandan Vyas <vyasnandu123@gmail.com>
2023-02-12 01:04:36 +0530

    minor change

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
 import '@/styles/globals.css'
+import React, { useEffect, useState } from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) 
+{
+  
+  const [user, setUser] = useState({ value: null });
+  // const [token, setToken] = useState("");
+    // console.log(messages);
+    useEffect(() => {
+      console.log("hey i am nandan useeffect from app.js");
+      const token = localStorage.getItem("token");
+      if (token) {
+      setUser({ value: token });
+    }
+  }, []);
+      // setToken(localStorage.getItem("token"));
+    //   console.log("hi this is useEFFect");
+    //   console.log(token);
+    // }, [token])
+    
+  return <Component {...pageProps} user={user} />
 }
