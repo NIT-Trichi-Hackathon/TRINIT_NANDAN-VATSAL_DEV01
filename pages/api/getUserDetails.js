20bdae457fe8c9ce19a9935b7d3c1e0a78a499b7
Nandan Vyas <vyasnandu123@gmail.com>
2023-02-11 23:51:33 +0530

    created API to get User Details

---
+++ pages/api/getUserDetails.js
@@ -0,0 +1,34 @@
+import Philanthropist from "@/models/Philanthropist";
+import Ngo from "@/models/Ngo";
+import connectDB from "../../middleware/mongoose";
+
+const handler = async (req, res) => 
+{
+  if (req.method == "POST") 
+  {
+    const jwt = require('jsonwebtoken');
+    const token = req.body.token;
+    const data = jwt.verify(token, process.env.JWT_Key);
+    // console.log(data.email)
+
+    if(req.body.category == "phl")
+    {
+        const userDetails = await Philanthropist.findOne({email: data.email});
+        res.status(200).json({ userDetails });
+    }
+    else if(req.body.category == "ngo")
+    {
+        const userDetails = await Ngo.findOne({email: data.email});
+        res.status(200).json({ userDetails });
+    }
+    else {
+        res.status(400).json({ error: "Error in category" });}
+    // const user = await User.findOne({ email: data.email });
+    // console.log(user);
+    // res.status(200).json({ user });
+  } else {
+    res.status(400).json({ error: "Error" });
+  }
+};
+
+export default connectDB(handler);
