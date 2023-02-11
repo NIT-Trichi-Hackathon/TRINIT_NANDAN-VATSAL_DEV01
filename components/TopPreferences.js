f7e1013a257b9bdfca2f25cb18e0cc27413a26c4
Nandan Vyas <vyasnandu123@gmail.com>
2023-02-11 12:36:35 +0530

    minor changes

--- components/TopPreferences.js
+++ components/TopPreferences.js
@@ -1,27 +1,27 @@
 import React from 'react'
 
 const TopPreferences = ({donationPreference,ngos}) => {
-    console.log(donationPreference)
+    // console.log(donationPreference)
   return (
     <>
-    <div>Top Preferences :</div>
+    <div className='text-2xl italic'>Top Preferences :</div>
     <div className="px-5 py-2 mx-auto">
     <div className='flex flex-wrap justify-start  space-x-2'>
     {ngos.map((item) => 
     {
-        console.log(item)
+        // console.log(item)
         if(donationPreference != item.target) return ;
         else 
               return (
                 <div key={item._id} className="space-y-3  w-1/4 px-4 border border-black">
                 {
                     donationPreference == item.target &&  <div>
-                    <div>{item.name}</div>
+                    <div className='font-semibold text-2xl underline capitalize'>{item.name}</div>
                     <div>
                     <div className='text-xl'>Future Plans :</div>
                     <ol className='list-decimal pl-3'>
                     {item.futurePlans.map((plan) => {
-                    return ( <li key={plan} className="text-xl">{plan}</li>)}) }
+                    return ( <li key={plan} className="text-sm">{plan}</li>)}) }
                     </ol>
                     </div>
 
@@ -29,7 +29,7 @@ const TopPreferences = ({donationPreference,ngos}) => {
                     <div className='text-xl'>History :</div>
                     <ol className='list-decimal pl-3'>
                     {item.history.map((hist) => {
-                    return ( <li key={hist} className="text-xl">{hist}</li>)}) }
+                    return ( <li key={hist} className="text-sm">{hist}</li>)}) }
                     </ol>
                     </div>
                     </div>
