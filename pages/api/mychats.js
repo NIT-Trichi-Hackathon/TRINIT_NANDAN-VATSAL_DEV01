2515decab27395686da62a0efdb588dc4e60ee27
Nandan Vyas <vyasnandu123@gmail.com>
2023-02-12 07:22:20 +0530

    Removed the bug with the message system

--- pages/api/mychats.js
+++ pages/api/mychats.js
@@ -6,7 +6,7 @@ const handler = async (req, res) => {
     const token =req.body.token;
     const data=jwt.verify(token,process.env.JWT_Key)
     // console.log(data.email)
-    let mychats = await inbox.find({email:data.email})
+    let mychats = await inbox.find({userEmail:data.email})
     // console.log(mydonations)
   res.status(200).json({ mychats });
 };
